68ff91f9ad5e0325473499b7a3e9892a21b795fc
Schumacher Itagiba Gonçalves <92530541+schumaz@users.noreply.github.com>
2025-08-12 14:38:22 -0300

    Add files via upload

---
+++ 1190.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main () {
+    int count=0;
+    double matrix[12][12], sum=0;
+    char input;
+
+    scanf("%c", &input);
+
+    for (int i = 0; i < 12; i++) {
+        for (int k = 0; k < 12; k++) {
+            scanf("%lf", &matrix[i][k]);
+        }
+    }
+
+    for (int i = 0; i < 12; i++) {
+        for (int k = 0; k < 12; k++) {
+            if (i < k && i + k >= 12) {
+                sum += matrix[i][k];
+                count++;
+            }
+        }
+    }
+
+    if (input == 'S') {
+        printf("%.1lf\n", sum);
+    } else {
+        printf("%.1lf\n", sum/count);
+    }
+
+    return 0;
+}
