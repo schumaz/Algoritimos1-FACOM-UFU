957d12ba5afbf8a9d8bb34717cffae60922733b6
Schumacher Itagiba Gonçalves <92530541+schumaz@users.noreply.github.com>
2025-09-04 23:16:19 -0300

    Add files via upload

---
+++ 2483.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+    int in;
+    scanf("%d", &in);
+
+    printf("Feliz nat");
+    for (int i = 0; i < in; i++) {
+        printf("a");
+    }
+    printf("l!\n");
+
+    return 0;
+}
