9b706d19649b02c12923e1ee485b65a144571b46
Schumacher Itagiba Gonçalves <92530541+schumaz@users.noreply.github.com>
2025-09-17 19:08:58 -0300

    Add files via upload

---
+++ 2755.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cmath>
+#include <string.h>
+
+int main() {
+    
+    printf("\"Ro'b'er\tto\\/\"\n");
+    printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") ('.')\n");
+
+    return 0;
+}
